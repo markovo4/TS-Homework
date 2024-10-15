5558afff171b17a104a88b6c988de1661782ed8e
markovo4 <obf2007@gmail.com>
2024-10-15 17:10:10 +0300

    hw-1

---
+++ src/index.js
@@ -0,0 +1,57 @@
+'use strict'
+
+class EventManager{
+    constructor() {
+        this.events = {}
+    }
+
+    on(eName, cb){
+        if(!this.events[eName]){
+            this.events[eName] = [];
+        }
+        this.events[eName].push(cb.bind(this));
+    }
+
+    off(eName, cb){
+        if(!this.events[eName]) return;
+
+        if(cb){
+            this.events[eName] = this.events[eName].filter((callback) =>callback === cb)
+        }
+
+        if(!cb){
+            delete this.events[eName];
+        }
+
+    }
+
+    trigger(eName, ...cb){
+        if(!this.events[eName]) return;
+
+        this.events[eName].forEach(callback =>{
+            callback(...cb);
+        })
+    }
+}
+
+const eventManager = new EventManager();
+
+const greetings = function(name){
+    console.log(`hello ${name}!`)
+}
+
+const goodBye = function(name){
+    console.log(`GoodBye ${name}!`)
+}
+
+eventManager.on('greetings',greetings);
+
+eventManager.trigger('greetings', 'Vlad')
+
+eventManager.on('goodBye',goodBye);
+
+eventManager.trigger('goodBye', 'Vlad')
+
+eventManager.off('goodBye', goodBye);
+
+eventManager.trigger('goodBye', 'Vlad')
