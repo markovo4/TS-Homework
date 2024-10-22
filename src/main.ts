26c531faac16bf5e055f5a305dfa4280c96976b3
Bla <obf2007@gmail.com>
2024-10-22 04:27:23 +0300

    hw-4

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  Hello world
-  </div>
-`
+type Product = {
+    name: string,
+    price: number,
+    available: boolean,
+}
+
+const fruit: Product = {
+    name: 'Pineapple',
+    price: 69,
+    available: true,
+}
+
+const productInfo = function(obj:Product):string{
+    return (`Product name: ${obj.name} 
+Product price: ${obj.price}
+Is available: ${obj.available}`)
+}
+
+console.log(productInfo(fruit))
