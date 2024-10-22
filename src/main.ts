a279c943a79230e27c14a6dfae90c38966ad150f
Bla <obf2007@gmail.com>
2024-10-22 04:17:29 +0300

    hw-3

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,11 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  Hello world
-  </div>
-`
+const aveCalc = function(numbers: Array<number>): number {
+    const total = numbers.reduce((acc: number, num: number):number =>{
+        return acc + num;
+    }, 0);
+
+    return total / numbers.length;
+}
+
+const grades: Array<number> = [10, 20, 20, 10, 20, 20, 10]
+
+console.log(aveCalc(grades))
