53f1f411d5d39e1eeeebb4f83b2a184b9079af29
markovo4 <obf2007@gmail.com>
2024-10-25 15:08:41 +0300

    HW-5

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,43 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  Hello world
-  </div>
-`
+type Product =  {
+    a: number,
+    b: Array<number>,
+    c: Product | number
+}
+
+const object:Product ={
+    a: 1,
+    b: [1, 2, 3],
+    c:{
+        a: 1,
+        b: [1, 2, 3],
+        c: 3
+    }
+}
+
+const deepClone = function<T>(obj: T):T{
+    if(obj === null || typeof obj !== 'object'){
+        return obj;
+    }
+
+    if (Array.isArray(obj)) {
+        const arr:Array<T> = [];
+
+        for (let i = 0; i < obj.length; i++) {
+            arr[i] = deepClone(obj[i]);
+        }
+        return arr as T;
+    }
+
+    const clonedObj: {[key: string]: any} ={};
+    for(const key in obj){
+        clonedObj[key] = deepClone(obj[key])
+    }
+    return clonedObj as T;
+
+
+}
+const obj2 = deepClone(object);
+console.log(obj2)
+console.log(obj2 === object)
+console.log(obj2.b === object.b)
+console.log(obj2.c === object.c)
