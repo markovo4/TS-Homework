2337a9b54ab95293ff3291004a2be082c39bf97a
markovo4 <obf2007@gmail.com>
2024-10-25 17:25:30 +0300

    HW-7

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,18 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  Hello world
-  </div>
-`
+const arr: Array<number> = [1, 2, 3, 4,5 , 6, 7, 8];
+
+console.log(arr);
+
+const reverseArray = function (arr: Array<number | string>): Array<number | string> {
+    const length = arr.length;
+    for (let i = 0; i < length / 2; i++) {
+
+        const temp = arr[i];
+        arr[i] = arr[length - 1 - i];
+        arr[length - 1 - i] = temp;
+    }
+    return arr;
+};
+
+console.log(reverseArray(arr));
+const reversedArr = reverseArray(arr);
+console.log(reversedArr === arr);
