92c6e94bd236df538d9c668e4e05ff15fe4ca45f
markovo4 <obf2007@gmail.com>
2024-10-26 17:57:49 +0300

    HW-8

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,31 @@
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-  Hello world
-  </div>
-`
+const textArea: HTMLTextAreaElement = document.createElement('textarea');
+const container: HTMLParagraphElement = document.createElement('p');
+const textAreaForbidden: HTMLTextAreaElement = document.createElement('textarea');
+const appContainer: HTMLDivElement | null = document.querySelector<HTMLDivElement>('#app');
+const badWords: Array<string> = [];
+
+const forbiddenWords = function(text: string, words: Array<string>): string {
+    words.forEach((word: string) => {
+        const regex = new RegExp(`\\b${word}\\b`, 'gi');
+        text = text.replace(regex, (matched) => `<del>${matched}</del>`);
+    });
+    return text;
+};
+
+const generateArray = function(e: Event): void {
+    const inputWords = (e.target as HTMLTextAreaElement).value
+        .replace(/[^\w\s]|_/g, '')
+        .split(' ')
+        .filter(word => word.length > 0);
+    badWords.push(...inputWords);
+};
+
+textArea.addEventListener('input', () => {
+    container.innerHTML = forbiddenWords(textArea.value, badWords);
+});
+
+textAreaForbidden.addEventListener('input', generateArray);
+
+appContainer?.append(textArea);
+appContainer?.append(textAreaForbidden);
+appContainer?.append(container);
