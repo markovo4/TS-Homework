ebd4c77c3625b108ba91868b6eed9caa885199a8
markovo4 <obf2007@gmail.com>
2024-11-09 18:09:03 +0200

    HW-10

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,6 @@
-type User = {
-    id: number;
-    name: string;
-    age: number;
-    isActive: boolean;
-};
-
-const filterByProperty = <T, K extends keyof T>(array: T[], key: K, value: T[K]): T[] => {
-    const newArray: T[] = []
-    array.forEach((obj)=>{
-        obj[key] === value && newArray.push(obj)
-    })
-    return newArray;
-}
+type User = { id: number; name: string; age: number; isActive: boolean; };
+type CallBack<T> = (value: T, index: number, array: T[]) => boolean;
+type FilterByPropertyFn = <T, K extends keyof T>(array: T[], key: K, value: T[K]) => T[];
 
 const users: User[] = [
     { id: 1, name: 'Alice', age: 25, isActive: true },
@@ -19,4 +8,19 @@ const users: User[] = [
     { id: 3, name: 'Charlie', age: 35, isActive: true },
 ];
 
-console.log(filterByProperty(users, 'isActive', true))
+const customFilter = <T>(array: T[], func: CallBack<T>): T[] => {
+    const filteredArray: T[] = [];
+
+    for (let i: number = 0; i <= array.length - 1; i++) {
+        if (func(array[i], i, array)) {
+            filteredArray.push(array[i]);
+        }
+    }
+    return filteredArray;
+};
+
+const filterByProperty: FilterByPropertyFn = function <T, K extends keyof T>(array: T[], key: K, value: T[K]): T[] {
+    return customFilter(array, (object) => object[key] === value);
+};
+
+console.log(filterByProperty(users, 'isActive', true));
