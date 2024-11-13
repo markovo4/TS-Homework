9ddd2409059ebc96b47f77ca35f7702a8e7a9048
markovo4 <obf2007@gmail.com>
2024-11-13 19:54:38 +0200

    practice

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,14 @@
-import {TypeFormatUserFn, TypeGetColorHex, TypeSumArray} from "./types/types.ts";
+import {
+    EnumOperator,
+    TypeAddTodoFn,
+    TypeAppConfig,
+    TypeAppConfigFn,
+    TypeCalculatorFn,
+    TypeFormatUserFn,
+    TypeGetColorHex,
+    TypeSumArray,
+    TypeTodo
+} from "./types/types.ts";
 import {EnumRgb} from "./enums/enums.ts";
 
 
@@ -33,3 +43,63 @@ const sumArray: TypeSumArray = (array) => {
 const arr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 console.log(sumArray(arr))
+
+
+const appConfig: TypeAppConfigFn = (appName, debugMode, maxUsers) => {
+    const obj: TypeAppConfig = {appName, debugMode, maxUsers};
+    return obj;
+}
+
+console.log(appConfig('Vova', true, 21))
+
+const calculator: TypeCalculatorFn = (a, b, operator) => {
+    switch (operator) {
+        case EnumOperator.Division:
+            return a / b;
+        case EnumOperator.Multiplication:
+            return a * b;
+        case EnumOperator.Minus:
+            return a - b;
+        case EnumOperator.Plus:
+            return a + b;
+        default:
+            return 'There is no such operation';
+    }
+}
+
+console.log(calculator(1, 2, EnumOperator.Division));
+
+const todos: TypeTodo[] = [];
+const todo: TypeTodo = {
+    id: 1,
+    title: 'Homework',
+    completed: true,
+};
+
+const isTypeTodo = (item: unknown): item is TypeTodo => {
+    if (typeof item !== 'object' || item === null) return false;
+
+    const {id, title, completed} = item as Record<string, unknown>;
+
+    return (
+        typeof id === 'number' &&
+        typeof title === 'string' &&
+        typeof completed === 'boolean'
+    );
+};
+
+
+const addTodo: TypeAddTodoFn = (todosList, singleTodo) => {
+
+    try {
+        if (!isTypeTodo(singleTodo)) {
+            throw new Error('Wrong type provided');
+        }
+        return [...todosList, singleTodo]
+    } catch (error) {
+        return (error as Error).message;
+    }
+}
+
+
+console.log(addTodo(todos, todo))
--- src/types/types.ts
+++ src/types/types.ts
@@ -5,3 +5,28 @@ export type TypeFormatUserFn = (firstName: string, lastName: string, middleName?
 export type TypeGetColorHex = (EnumColors: EnumRgb) => string;
 
 export type TypeSumArray = (array: number[]) => number;
+
+export type TypeAppConfig = {
+    appName: string,
+    debugMode: boolean,
+    maxUsers: number
+}
+
+export type TypeAppConfigFn = (appName: string, debugMode: boolean, maxUsers: number) => TypeAppConfig | 'Error';
+
+export const enum EnumOperator {
+    Plus = '+',
+    Minus = '-',
+    Division = '/',
+    Multiplication = '*',
+}
+
+export type TypeCalculatorFn = (a: number, b: number, operator: EnumOperator) => number | string;
+
+export type TypeTodo = {
+    id: number,
+    title: string,
+    completed: boolean,
+}
+
+export type TypeAddTodoFn = (todos: TypeTodo[], todo: TypeTodo) => Array<TypeTodo> | string;
